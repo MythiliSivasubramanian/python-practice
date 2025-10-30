3fc3041d03bb49409b37dc4c3d5b1b03329acf16
MythiliSivasubramanian <sivasubramanianmythili@gmail.com>
2025-10-30 09:39:10 +0100

    Added favourite_color.py program
    This program asks the user for their name and favourite color,
    then prints a friendly greeting message using f-strings.

---
+++ M1_Basics/M1_Basics/favourite_color.py
@@ -0,0 +1,4 @@
+# Get details from user and print it
+user_name = input("Enter your name: ")
+color = input("Enter your favourite color: ")
+print(f"Hello {user_name}, your favourite color is {color}. Good day!")
