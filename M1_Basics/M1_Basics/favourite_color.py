43e3cce416293046affd204029b90f70506ddd85
MythiliSivasubramanian <sivasubramanianmythili@gmail.com>
2025-10-30 10:05:34 +0100

    Delete M1_Basics/M1_Basics directory

--- M1_Basics/M1_Basics/favourite_color.py
+++
@@ -1,4 +0,0 @@
-# Get details from user and print it
-user_name = input("Enter your name: ")
-color = input("Enter your favourite color: ")
-print(f"Hello {user_name}, your favourite color is {color}. Good day!")
