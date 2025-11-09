5b2ef8fa8d9c2f2544a722273b536885a66ab838
MythiliSivasubramanian <sivasubramanianmythili@gmail.com>
2025-11-09 21:21:34 +0100

    Added remove_duplicates_items_list.py
    simple Program to remove duplicates in the list. 
    
    Functions
    for Loop
    if (not in)

---
+++ M4_Lists/remove_duplicates_items_list.py
@@ -0,0 +1,12 @@
+# To remove duplicates items in the list
+
+def remove_duplicates(lst):
+    unique_list = []
+    for item in lst:
+        if item not in unique_list:
+            unique_list.append(item)
+    return unique_list
+
+    items = [1, 2, 2, 3, 4, 4, 5, 1]
+    print("Original list:", items)
+    print("After removing duplicates:", remove_duplicates(items))
