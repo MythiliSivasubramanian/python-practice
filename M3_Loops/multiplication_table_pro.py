f78f1916b2689d00383614e7c7ca4375e3ba8229
MythiliSivasubramanian <sivasubramanianmythili@gmail.com>
2025-11-14 13:18:44 +0100

    Updated multiplication_table_pro.py
    Updated the program with clearn output formatting

--- M3_Loops/multiplication_table_pro.py
+++ M3_Loops/multiplication_table_pro.py
@@ -1,54 +1,78 @@
-# Multiplication table generater.
+# Multiplication table generator
 import string
 
 
-# Function to perform the Multiplication
-def multiplication_calc(start,end):
+# 2.3.1 Multiplication table generater
+def  multiplication_table(startnum,stopnum):
     total = 0
     table = []
-    for i in range(1,end+1):
-        total = i * start
-        table.append(f"{i} * {start} = {total}")
+    for i in range(1,stopnum+1):
+        total = i * startnum
+        table.append(f"{i} * {startnum} = {total}")
     return table
 
-# Function to get and validate the Input function
-def get_input(prompt_1,prompt_2):
+# 2.2.1 Define tables function
+def start_end_tables(starttable,endtable,endnumber):
+    all_tables = []
+    for i in range(starttable,endtable + 1):
+        # Call Multiplication table function to generate the table for i
+        table = multiplication_table(i,endnumber)
+        all_tables.append(f"\nTable of {i}")
+        all_tables.append("----------------")
+        all_tables.extend(table)
+
+        if i != endtable:
+            all_tables.append("\n****************\n")
+    return all_tables
+
+
+
+
+
+# 2.1.1 Define input function
+def get_input(prompt1,prompt2,prompt3):
     while True:
         try:
-            user_start_number = int(input(prompt_1))
-            if user_start_number > 0:
-                user_end_number = int(input(prompt_2))
-                if user_end_number > 0:
-                    return user_start_number,user_end_number
-                else:
-                    print("Invalid End number!. Please enter a number greater than 0")
+            start_table = int(input(prompt1))
+            if start_table > 0:
+                tables_end = int(input(prompt2))
+                if tables_end >= start_table:
+                    end_num = int(input(prompt3))
+                    if end_num > 0:
+                        return start_table,tables_end,end_num
             else:
-                print("Invalid Input! Enter a valid number greater than 0!")
-
+                print("Invalid number. Please enter a number greater than 0!")
         except ValueError:
-             print("Invalid Input! Please enter a valid number greater than 0! ")
+            print("Invalid Input. Please enter a number greater than 0!")
 
-# main function definition
+
+
+
+
+# 2. Define Main function
 def main():
+    print("Multiplication Table generator. \nEg If start Table number as 2, end table number as 5, end number as 10, it would prints tables of 2,3,4,5 until 10 each table")
     while True:
+        prompt_text_1 = "\nEnter the start table number : "
+        prompt_text_2 = "\nEnter the end table number : "
+        prompt_text_3 = "\nUntil how many numbers you want for each table : "
 
-        # Prompt and validate the user input, start and end number for a multiplication table.
-        prompt_text_1 = "Enter a number to see its multiplication table : "
-        prompt_text_2 = "Until how many numbers the multiplication table to be generated : "
-        start_num,end_num = get_input(prompt_text_1,prompt_text_2)
 
-        # Call the function to perform the multiplication table
-        result_table = multiplication_calc(start_num,end_num)
+        # 2.1 Call Input function
+        table_start,end_table,end_number = get_input(prompt_text_1,prompt_text_2,prompt_text_3)
 
-        # Print the results
-        print("\n".join(result_table))
+        # 2.2 Call Multiplication function
+        result = start_end_tables(table_start,end_table,end_number)
+
+        # 2.3 prints tables
+        print("\n".join(result))
 
         # Choice to check for another multiplication table
-        choice = input("Would you like to check for another number (yes/no) : ").lower().strip().strip(string.punctuation)
+        choice = input("\nWould you like to check for another number (yes/no) : ").lower().strip().strip(string.punctuation)
         if choice == "no":
-           print("Thank you and Good day!")
-           break
+            print("Thank you and Good day!")
+            break
 
-# call the main function
-if __name__ =="__main__":
+# 1. Call main function
+if __name__ == "__main__":
     main()
