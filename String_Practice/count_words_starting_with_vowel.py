5269f13927981839b36ce466ec992f19dbed8dd7
MythiliSivasubramanian <sivasubramanianmythili@gmail.com>
2025-11-25 09:38:09 +0100

    Added count_words_starting_with_vowel.py
    This program counts the no of words  starting with the Vowel in the given Input.
    
    For loop
    if

---
+++ String_Practice/count_words_starting_with_vowel.py
@@ -0,0 +1,12 @@
+""" Count words that start with a vowel in a sentence . Counts how many words start with a vowel (a, e, i, o, u)
+Ignore case Sample Input: I am learning Python and it is fun . Sample Output : Words starting with a vowel: 5 """
+
+user_string = input("Enter a string : ").lower().split()
+vowels = "aeiou"
+count = 0
+
+for word in user_string:
+    if word and word[0] in vowels:
+        count += 1
+
+print("Words starting with a vowel:", count)
