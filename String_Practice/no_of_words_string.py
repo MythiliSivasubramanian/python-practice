7792fcbc636811297288112569efef694de6e9f4
Mythili Sivasubramanian <sivasubramanianmythili@gmail.com>
2025-11-28 10:53:53 +0100

    Add word count functionality in no_of_words_string.py
    This script counts the number of words in a user-provided string by splitting it based on spaces.

---
+++ String_Practice/no_of_words_string.py
@@ -0,0 +1,5 @@
+
+""" Count the number of words in a string. Sample Input: "I love Python programming" Sample Output: 4 """
+
+user_string = input("Enter a String separated by sapces between the words : ").split(" ")
+print(len(user_string))
