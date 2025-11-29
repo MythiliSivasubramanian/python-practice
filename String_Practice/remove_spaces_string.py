cddce1c27caf0688c3451e7d9382ffd742d369c0
Mythili Sivasubramanian <sivasubramanianmythili@gmail.com>
2025-11-29 21:54:55 +0100

    Added script to remove spaces from a string
    This script removes all spaces from a user-provided string.

---
+++ String_Practice/remove_spaces_string.py
@@ -0,0 +1,5 @@
+""" Remove all spaces from a string.  Sample Input: I love Python  Sample Output: "IlovePython """
+import string
+
+user_input = input("Enter a String with spaces : ")
+print(user_input.replace(" ",""))
