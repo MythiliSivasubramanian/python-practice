64e96110f4b0f9170403a099c73def7316793b77
Mythili Sivasubramanian <sivasubramanianmythili@gmail.com>
2025-12-03 14:15:41 +0100

    added vowel_count.py (manual method)
    vowel-counting loop example (manual method)

---
+++ Loops/vowels_count.py
@@ -0,0 +1,11 @@
+# Countr vowels in a string 
+
+text = input("Enter a string : ")
+vowels = "aeiou"
+count = 0
+
+for ch in text:
+    if ch.lower() in vowels:
+        count += 1
+
+print("Number of vowels:", count)
