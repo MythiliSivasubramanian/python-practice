3024e51932748ff9f106136fce5c4c65d4d5dc0e
Mythili Sivasubramanian <sivasubramanianmythili@gmail.com>
2025-12-06 20:49:08 +0100

    Created prime_num.py

---
+++ Functions/prime_num.py
@@ -0,0 +1,10 @@
+def is_prime(n):
+    """Return True if n is a prime number."""
+    if n <= 1:
+        return False
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+print(is_prime(11))
