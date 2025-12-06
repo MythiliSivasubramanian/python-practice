326fa8d59125bd11505840fc6eb82babfef8c808
Mythili Sivasubramanian <sivasubramanianmythili@gmail.com>
2025-12-06 20:51:41 +0100

    Create Celsius_Fahrenheit.py

---
+++ Functions/Celsius_Fahrenheit.py
@@ -0,0 +1,10 @@
+def c_to_f(c):
+    """Convert Celsius to Fahrenheit."""
+    return (c * 9/5) + 32
+
+def f_to_c(f):
+    """Convert Fahrenheit to Celsius."""
+    return (f - 32) * 5/9
+
+print(c_to_f(25))
+print(f_to_c(77))
