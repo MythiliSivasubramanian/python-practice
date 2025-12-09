70734b46c17ee098b93ffc571b2613cf011cb654
Mythili Sivasubramanian <sivasubramanianmythili@gmail.com>
2025-12-09 13:26:49 +0100

    Added extract_1st_last_char.py
    Extracted the 1st and last characters from the given input and printed using concatenate operator
    
    Accessing characters using Indicies
    concatenate operator

---
+++ String_Practice/extract_1st_last_char.py
@@ -0,0 +1,6 @@
+""" Given a string, extract the first and last character and print them together.
+Sample input is: Python Sample Output should be: Pn """
+
+
+my_string = input("Enter a word : ")
+print(my_string[0]+my_string[-1])
