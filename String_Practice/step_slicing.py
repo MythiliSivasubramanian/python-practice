b2db3993e5ce73cf48a9a0f2fcdc598ca1fd78b0
Mythili Sivasubramanian <sivasubramanianmythili@gmail.com>
2025-12-10 15:01:41 +0100

    Added step_slicing.py
    Step Slicing

---
+++ String_Practice/step_slicing.py
@@ -0,0 +1,5 @@
+"""Step Slicing : Print every 2nd character from index 1 to 12.Sample Input: PythonProgramming
+sample Output: yhnrga """
+
+user_input = input("Enter a word : ")
+print(user_input[1:13:2])
