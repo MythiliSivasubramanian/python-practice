316cc30a4c5816526b5f925280f5faec6ec4dfbd
Mythili Sivasubramanian <sivasubramanianmythili@gmail.com>
2025-12-10 14:58:28 +0100

    Added negative_index_reverse.py
    Negative index
    reversing

---
+++ String_Practice/negative_index_reverse.py
@@ -0,0 +1,6 @@
+""" Using negative indices only, print substring from index -5 to -1 in reverse. Sample Input: Programming
+Sample Output: gnimm  """
+
+
+user_input = input("Enter a string : ")
+print(user_input[-1:-6:-1])
