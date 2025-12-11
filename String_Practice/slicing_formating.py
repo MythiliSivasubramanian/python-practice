d43dab491276e7e1095712fe1596175914c905c0
Mythili Sivasubramanian <sivasubramanianmythili@gmail.com>
2025-12-11 09:41:56 +0100

    Created slicing_formating.py
    To extract a Substring from a  String and formatting it using f-string
    
    Slicing
    f-String formating

---
+++ String_Practice/slicing_formating.py
@@ -0,0 +1,5 @@
+""" Combine Slicing and Formatting Given: my_string = "PythonProgramming"   Extract "Programming"  Use an f-string to print:
+I love Programming """
+
+my_string = "PythonProgramming" 
+print(f"I love {my_string[6:]}")
