c7d1cc2719532b755cac2fcab026d0d7085d244c
Mythili Sivasubramanian <sivasubramanianmythili@gmail.com>
2025-12-11 09:48:14 +0100

    Created formatting_fstring_decimal_values.py
    Taking name, age and height as input and printing using fstring formatting and restricting the height values to 2 dight decimal values
    
    f-string

---
+++ String_Practice/formatting_fstring_decimal_values.py
@@ -0,0 +1,8 @@
+""" Get name , age and height as input 
+Use an f-string to print: My name is name, I am age years old and 0.00 meters tall. The Height is to be shown with 2 decimal places. """
+
+users_name = input("Enter Name : ")
+users_age = int(input("Enter age : ")
+users_height = float(input("Enter height (meters) in decimal eg 1.68 : ")
+
+print(f"My name is {users_name}, I am {users_age} years old and {users_height :.2f} meters tall.")
