72315e1c1532b7ddc1d0c815dcf53f746425598b
Mythili Sivasubramanian <sivasubramanianmythili@gmail.com>
2025-12-16 12:03:05 +0100

    Updated count_vowels.py
    Updated the comments and corrected the code

--- Basics/count_vowels.py
+++ Basics/count_vowels.py
@@ -1,13 +1,18 @@
 # Count vowels in a given string
 
-def count_vowels(text):
-    vowels = input("Enter a string")
+def count_vowels():
+    text = input("Enter a string to count the vowels in it : ")
     count = 0
+    vowels = "AEIOUaeiou" # vowels 
 
     for char in text:
         if char in vowels:
             count += 1
-
     return count
 
-print(count_vowels())
+# Call the function and asssign the return value of the fun() to a variable
+vowels_count = count_vowels()
+
+# Print result
+print(F"Vowels count is : {vowels_count}")
+
