ab06d3e5bbf4699418f6c46f85e627119af0ddab
Mythili Sivasubramanian <sivasubramanianmythili@gmail.com>
2025-12-16 11:50:30 +0100

    Updated Remove_non_alpha_chars.py
    Added comments

--- String_Practice/Remove_non_alpha_chars.py
+++ String_Practice/Remove_non_alpha_chars.py
@@ -3,13 +3,15 @@ Sample output : Python """
 
 my_string = "<<<Python>>>"
 # Method 1
-print(my_string[3:9])
+print(my_string[3:9]) # Slicing. End index is exclusive
 
 # Method 2 
 users_string = input("Enter a String with symbols  : ")
 alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
 new_string = ""
+
 for ch in users_string:
 	if ch in alpha:
-		new_string += ch
+		new_string += ch  
+		# strings are immutable. New objects are created for each and every modification. Hence not memory and exe time efficient.
 print(new_string)
