faaf2ab395f6b7c630b9973784b964cd75f00c9d
Mythili Sivasubramanian <sivasubramanianmythili@gmail.com>
2025-12-21 20:16:48 +0100

    Create basic_dict_accessing_keys.py

---
+++ Dictionaries/basic_dict_accessing_keys.py
@@ -0,0 +1,11 @@
+#Basic dictionary creation and access
+
+student = {
+    "name": "Rose",
+    "age": 22,
+    "course": "Python"
+}
+
+print(student["name"])
+print(student["age"])
+print(student["course"])
