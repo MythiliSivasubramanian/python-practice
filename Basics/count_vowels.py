dfbb6b6e01cd6f4df451988a7374f7f053fc132f
Mythili Sivasubramanian <sivasubramanianmythili@gmail.com>
2025-12-28 21:35:51 +0100

    Added comments.
    - Added inline comments to explain how vowels are counted
    - Clarified handling of uppercase and lowercase characters
    - No functional changes to the code

--- Basics/count_vowels.py
+++ Basics/count_vowels.py
@@ -1,18 +1,21 @@
 # Count vowels in a given string
 
 def count_vowels():
-    text = input("Enter a string to count the vowels in it : ")
-    count = 0
-    vowels = "AEIOUaeiou" # vowels 
+    # get the input string
+    text = input("Enter a string to count the vowels in it : ") 
+    count = 0 # Initial count of vowels is 0
+    vowels = "AEIOUaeiou" #  # includes both uppercase and lowercase vowels 
 
+    # Loop to count the vowels from the input 'text'
     for char in text:
         if char in vowels:
             count += 1
+            # return total number of vowels found
     return count
 
 # Call the function and asssign the return value of the fun() to a variable
 vowels_count = count_vowels()
 
 # Print result
-print(F"Vowels count is : {vowels_count}")
+print(f"Vowels count is : {vowels_count}")
 
