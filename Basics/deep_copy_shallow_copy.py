e16a629057e633018db1b5e29b4ab1966ad43902
Mythili Sivasubramanian <sivasubramanianmythili@gmail.com>
2025-12-28 21:44:00 +0100

    Added comments
    - Added comments
    - No changes in code logic

--- Basics/deep_copy_shallow_copy.py
+++ Basics/deep_copy_shallow_copy.py
@@ -1,12 +1,12 @@
 """   Create 3 variables referencing the same list. Mutate one. Check others."""
 import copy
 
-list_1 = list_2 = list_3 = [1,2,3]
-# chained assignements. 
+list_1 = list_2 = list_3 = [1, 2, 3]
+# chained assignements. (all variables reference the same list)
 
 list_shallowcopy = list_1.copy()   # Shallow copy duplicates only the top-level container, inner objects are shared.
 
-list_deep = copy.deepcopy(list_1) # A deep copy creates a new outer object AND duplicates all inner objects recursively. Nothing is shared
+list_deep = copy.deepcopy(list_1) #  Deep copy: creates a completely independent copy
 
 list_1.append(4)   # List 1: [1, 2, 3, 4] List 2: [1, 2, 3, 4] List 3: [1, 2, 3, 4]
 
