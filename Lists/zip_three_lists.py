0e8f7d0fe94b538879c1bcfeca5e48928693d720
Mythili Sivasubramanian <sivasubramanianmythili@gmail.com>
2025-12-28 13:10:05 +0100

    zip three predefined lists
    Zipped three lists into a list of tuples.

---
+++ Lists/zip_three_lists.py
@@ -0,0 +1,10 @@
+# Zip() three lists
+
+# Predefined lists
+names = ['Alice', 'Bob', 'Charlie']
+ages = [24, 30, 22]
+countries = ['USA', 'UK', 'Canada']
+
+info = list(zip(names,ages,countries))
+print("\nName,age,country : ", info)
+
