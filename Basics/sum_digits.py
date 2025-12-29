ea0e1e6f2d80d079a3a181bd81854199bb57e4f2
Mythili Sivasubramanian <sivasubramanianmythili@gmail.com>
2025-12-29 22:56:54 +0100

    updated comments
    updated comments

--- Basics/sum_digits.py
+++ Basics/sum_digits.py
@@ -1,5 +1,7 @@
 def sum_of_digits(n):
-    """Return the sum of digits of n."""
+    
+    """Return the sum of digits of n (assuming n as a non-negative integer)."""
     return sum(int(d) for d in str(n))
 
+# Test the function with some random sample number
 print(sum_of_digits(9875))
