4251f43c7b2497461611bc4480394b5da5c30323
Mythili Sivasubramanian <sivasubramanianmythili@gmail.com>
2025-12-29 23:00:45 +0100

    Updated comments for more clarity
    Updated comments for more clarity

--- Basics/vowels_consonants_count.py
+++ Basics/vowels_consonants_count.py
@@ -1,11 +1,17 @@
 # Count vowels and consonants in a string
 
 def count_vowels_consonants(text):
+    # Both upper and lower case vowels 
     vowels = "aeiouAEIOU"
+
+    # Inital count of vowels (both cases) as 0
     vowels_count = 0
+
+    # Inital count of consonants as 0
     consonants_count = 0
 
     for char in text:
+        # .isalpha() considers only alphabetic characters
         if char.isalpha():
             if char in vowels:
                 vowels_count += 1
@@ -13,4 +19,5 @@ def count_vowels_consonants(text):
                 consonants_count += 1
     return vowels_count, consonants_count
 
+# Testing the function with sample string
 print(count_vowels_consonants("Test Message"))
