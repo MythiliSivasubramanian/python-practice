93d709acaa396d0b37ca73c148227b18767623cf
Mythili Sivasubramanian <sivasubramanianmythili@gmail.com>
2025-12-29 22:53:13 +0100

    Enhance comments in smallest_positive_number.py
    Added comments for more clarity

--- Basics/smallest_positive_number.py
+++ Basics/smallest_positive_number.py
@@ -1,13 +1,16 @@
 # Find the smallest positive number from a list
 
 def smallest_positive(nums):
+    # stores the smallest positive number found so far
     smallest = None
 
     for n in nums:
+        # consider only positive numbers only
         if n > 0:
             if smallest is None or n < smallest:
                 smallest = n
 
     return smallest
 
+# Test the function with some sample input
 print(smallest_positive([3, -1, 0, 5, 2, -4]))
