b932ce742c69e99ca97b0e3e0cae8dd69622f12f
Mythili Sivasubramanian <sivasubramanianmythili@gmail.com>
2025-12-29 22:50:39 +0100

    Added comments
    Added  comments

--- Basics/remove_duplicates.py
+++ Basics/remove_duplicates.py
@@ -2,8 +2,10 @@ def remove_duplicates(items):
     """Return a new list with duplicates removed."""
     unique = []
     for i in items:
+        # add item only if not already seen
         if i not in unique:
             unique.append(i)
     return unique
 
+# Test the function with some sample input
 print(remove_duplicates([1, 2, 2, 3, 4, 4, 5]))
