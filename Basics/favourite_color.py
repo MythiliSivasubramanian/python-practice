99a19da60c997d5319728b77ec75ee123c7225e0
Mythili Sivasubramanian <sivasubramanianmythili@gmail.com>
2025-12-29 22:47:01 +0100

    Add comments for user input for favorite color and greeting
    Add comments for user input for favorite color and greeting

--- Basics/favourite_color.py
+++ Basics/favourite_color.py
@@ -1,4 +1,10 @@
 # Get details from user and print it
+
+ # storing user's name
 user_name = input("Enter your name: ")
+
+# storing user's favourite color
 color = input("Enter your favourite color: ")
+
+# Displaying a greeting message
 print(f"Hello {user_name}, your favourite color is {color}. Good day!")
