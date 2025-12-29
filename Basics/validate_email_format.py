fb3e67726efe2f72748460bbb4ab10f1dbe07b73
Mythili Sivasubramanian <sivasubramanianmythili@gmail.com>
2025-12-29 22:58:20 +0100

    Updated comments for more clarity
    Updated comments for more clarity

--- Basics/validate_email_format.py
+++ Basics/validate_email_format.py
@@ -1,5 +1,6 @@
 def is_valid_email(email):
-    """Simple email validation check."""
+    """Very Simple email validation check."""
     return "@" in email and "." in email and len(email) > 5
 
+# Testing the function with some sample data
 print(is_valid_email("user@example.com"))
