6b698afb72d7ecd10e2800f1e91db83db23c5a55
Mythili Sivasubramanian <sivasubramanianmythili@gmail.com>
2025-12-29 22:54:28 +0100

    Add comments to square_numbers function
    Added comments to clarify the purpose of the function and the test.

--- Basics/squares:of_numbers.py
+++ Basics/squares:of_numbers.py
@@ -1,9 +1,11 @@
 # Find squares of numbers in a list
 
 def square_numbers(nums):
+    # list to store squared values
     squares = []
     for n in nums:
         squares.append(n ** 2)
     return squares
 
+# Test the function with some random sample list of numbers
 print(square_numbers([1, 2, 3, 4, 5]))
