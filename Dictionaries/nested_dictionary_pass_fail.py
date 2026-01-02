0e47a555b378a192627f8cbf4a006036dcf4b0be
Mythili Sivasubramanian <sivasubramanianmythili@gmail.com>
2026-01-02 23:55:01 +0100

    Add nested dictionary
    -iterating over a nested dictionary using for-loops
    -Accessing outer keys (name) and inner keys–value pairs (subjects & scores)
    - Applying filter / logic (scores > 60)

---
+++ Dictionaries/nested_dictionary_pass_fail.py
@@ -0,0 +1,24 @@
+""" Nested Dictionaries – Pass / Fail Conversion
+Converting student subject scores either as "Pass" or "Fail". Pass if score >= 60
+"""
+
+# Predefined nested dictionary
+students = {"Alice": {"math": 85, "science": 90}, "Bob": {"math": 70, "science": 60}}
+
+# New dictionary to store results with score as pass or fail
+result = {}
+
+# Outer loop: student name and respective subject dictionary
+for name, inner_dict in students.items():
+    # creating inner dictionary for each student
+    result[name] = {}  
+
+    # Inner loop: subject and score
+    for subject, score in inner_dict.items():
+        if score >= 60:
+            result[name][subject] = "Pass"
+        else:
+            result[name][subject] = "Fail"
+
+# Final output
+print("Result:",result)
