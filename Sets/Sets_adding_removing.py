b1059f0dd3062d3e5b5d0efbd004d926bfff0546
Mythili Sivasubramanian <sivasubramanianmythili@gmail.com>
2026-01-09 22:14:09 +0100

    Praticed the various basics functions and operations of Sets

---
+++ Sets/Sets_adding_removing.py
@@ -0,0 +1,11 @@
+# Program to demonstrate basic set operations
+
+fruits = {"apple", "banana", "cherry"}
+
+# Adding an element
+fruits.add("orange")
+
+# Removing an element
+fruits.remove("banana")
+
+print("Final set:", fruits)
