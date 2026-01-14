0177f613c21b48917cc542b960d1ecc6fa508e27
Mythili Sivasubramanian <sivasubramanianmythili@gmail.com>
2026-01-14 21:33:27 +0100

    Demonstrates intersection using '&' operator

---
+++ Sets/intersection_basic_sets.py
@@ -0,0 +1,14 @@
+# Basic Set Intersection
+
+
+# Create 2 sets a and b with 2 common elements
+set_a = {1, 2, 3, 4, 5}
+set_b = {4, 5, 6, 7}
+
+# & gives the common elements from both sets
+common = set_a & set_b
+
+# Printing the sets a and b
+print("\nSet A:", set_a)
+print("\nSet B:", set_b)
+print("\nCommon Elements set_a & set_b : ", common,"\n")
