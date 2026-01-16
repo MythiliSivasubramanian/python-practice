a0c9056b3bbb45f7ae178cfe8e9b3f0217e37636
Mythili Sivasubramanian <sivasubramanianmythili@gmail.com>
2026-01-16 21:06:57 +0100

    Find elements present only in Set A

---
+++ Sets/difference.py
@@ -0,0 +1,10 @@
+# Program: Find elements present only in Set A
+
+set_a = {1, 2, 3, 4, 5}
+set_b = {4, 5, 6, 7}
+
+only_in_a = set_a - set_b
+
+print("Set A:", set_a)
+print("Set B:", set_b)
+print("Only in Set A:", only_in_a)
